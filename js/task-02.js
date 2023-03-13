8c7dbc2af12f3c865d8f2f912622cf17a7c7a6b3
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-03-13 19:35:07 +0200

    done Task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const createOneIngredient = ingredients.map((ingredient) => {
+  const oneIngredientEl = document.createElement("li");
+  oneIngredientEl.textContent = ingredient;
+  oneIngredientEl.classList.add("item");
+  return oneIngredientEl;
+});
+
+ingredientsEl.append(...createOneIngredient);
