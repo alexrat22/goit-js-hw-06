30d2c11e6bfaba5317ac4d2bbc86d1a02abfa90a
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-03-14 17:11:36 +0200

    Update Task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,24 +1,19 @@
-const counter = {
-  counterValue: 0,
-  decrement() {
-    this.counterValue -= 1;
-  },
-
-  increment() {
-    this.counterValue += 1;
-  },
-};
+let counterValue = 0;
 
 const valueEl = document.querySelector("#value");
 
+function decrement() {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+}
+
+function increment() {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+}
+
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
-decrementBtn.addEventListener("click", function () {
-  counter.decrement();
-  valueEl.textContent = counter.counterValue;
-});
+decrementBtn.addEventListener("click", decrement);
 
 const incrementBtn = document.querySelector('button[data-action="increment"]');
-incrementBtn.addEventListener("click", function () {
-  counter.increment();
-  valueEl.textContent = counter.counterValue;
-});
+incrementBtn.addEventListener("click", increment);
