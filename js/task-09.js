0ebd51561d8540648e4604271d4757a40ad42d54
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-03-16 10:21:12 +0200

    done Task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const backgroundArea = document.querySelector(".widget");
+const colorName = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+changeColorButton.addEventListener("click", onChangeColorButtonClick);
+
+function onChangeColorButtonClick(event) {
+  colorName.textContent = getRandomHexColor();
+  backgroundArea.style.backgroundColor = colorName.textContent;
+}
